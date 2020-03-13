177b7572944184a509cbd11b52d0b1285dd32673
Caca Wilda Andika <akikazuandika@gmail.com>
2020-03-13 18:56:56 +0700

    middleware admin

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(empty($request->session()->get("id"))) return redirect("admin/login");
+        return $next($request);
+    }
+}
