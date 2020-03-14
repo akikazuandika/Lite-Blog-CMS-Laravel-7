e917cb87872a8395d3014bdaf1ad012cf268782a
Caca Wilda Andika <akikazuandika@gmail.com>
2020-03-14 15:25:10 +0700

    category and tags select

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -10,6 +10,9 @@
 <div class="row">
     <!-- summernote -->
     <link rel="stylesheet" href="{{ asset('plugins/summernote/summernote-bs4.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('plugins/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
     <div class="col-md-12">
         <div class="card card-outline card-info">
             <div class="card-header">
@@ -19,43 +22,87 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body pad">
-                <div class="mb-1">
-                    <div class="form-group col-sm-6">
-                        <label>Title</label>
-                        <input type="text" id="title" class="form-control" placeholder="Enter the title ...">
+                <div class="row mb-2">
+                    <div class="col col-md-8">
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" id="title" class="form-control" placeholder="Enter the title ...">
+                        </div>
+                    </div>
+
+                    <div class="col col-md-4">
+                        <div class="form-group">
+                            <label>Category</label>
+                            <select class="form-control select2bs4" style="width: 100%;">
+                              <option class="disabled" selected="selected" disabled>Select Category</option>
+                              <option>Alaska</option>
+                              <option>California</option>
+                              <option>Delaware</option>
+                              <option>Tennessee</option>
+                              <option>Texas</option>
+                              <option>Washington</option>
+                            </select>
+                          </div>
                     </div>
                 </div>
-                <div class="mb-3">
-                    <div class="form-group col-sm-6">
-                        <label>Url</label>
-                        <input type="text" id="slug" class="form-control" placeholder="Enter the title ...">
+                <div class="row mb-2">
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <label>Url</label>
+                            <input type="text" id="slug" class="form-control" placeholder="Enter the title ...">
+                        </div>
                     </div>
-                </div>
-                <div class="mb-3">
-                    <div class="form-group col-sm-6">
-                        <label for="inputThumbnail">File input</label>
-                        <div class="input-group">
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="inputThumbnail">
-                                <label class="custom-file-label" for="inputThumbnail">Choose file</label>
-                            </div>
-                            <div class="input-group-append">
-                                <span class="input-group-text" id="">Upload</span>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label>Tags</label>
+                            <div class="select2-purple">
+                                <select class="select2bs4" multiple="multiple" data-placeholder="Select a State"
+                                    style="width: 100%;">
+                                    <option>Alabama</option>
+                                    <option>Alaska</option>
+                                    <option>California</option>
+                                    <option>Delaware</option>
+                                    <option>Tennessee</option>
+                                    <option>Texas</option>
+                                    <option>Washington</option>
+                                </select>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="mb-3 col-sm-12">
-                    <textarea id="content" class="textarea" placeholder="Place some text here"
-                        style="width: 100%; height: 400px; font-size: 14px; line-height: 16px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                        <p class="text-sm mb-0">
-                            Editor <a href="https://github.com/bootstrap-wysiwyg/bootstrap3-wysiwyg">Documentation and license
-                                information.</a>
-                        </p>
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="inputThumbnail">File input</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="inputThumbnail">
+                                    <label class="custom-file-label" for="inputThumbnail">Choose file</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="">Upload</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3 col-md-6">
-                    <button onclick="send()" class="btn btn-primary">Save</button>
-                    <button onclick="send(true)" class="btn btn-primary">Save & Publish</button>
+                <div class="mb-3 row">
+                    <div class="col-md-12">
+                        <textarea id="content" class="textarea" placeholder="Place some text here"
+                        style="width: 100%; height: 400px; font-size: 14px; line-height: 16px; border: 1px solid #dddddd; padding: 10px;"></textarea>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="text-sm mb-0">
+                            Editor <a href="https://github.com/bootstrap-wysiwyg/bootstrap3-wysiwyg">Documentation and license information.</a>
+                        </p>
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-md-12">
+                        <button onclick="send()" class="btn btn-primary">Save</button>
+                        <button onclick="send(true)" class="btn btn-primary">Save & Publish</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -65,11 +112,18 @@
 
 <!-- Summernote -->
 <script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
+<!-- Select2 -->
+<script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
 <script>
     $(function () {
         // Summernote
         $('.textarea').summernote({
             height : 200
+        })
+
+        //Initialize Select2 Elements
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
         })
     })
 
